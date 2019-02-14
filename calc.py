bd31e459abfb36f7c119acf985ff6e08abd112cc
Raphael Costa <rapha2706@hotmai..com>
2019-02-14 10:44:38 -0200

    Modificando o .join por .replace e retirando comentarios

--- calc.py
+++ calc.py
@@ -1,52 +1,44 @@
 test = input("Digite a equação: ")
-test = test.split()
-test = "".join(test)
+test = test.replace(" ", "")
 
 clean = []
 comeco = 0
 for i in range(len(test)):
-    # print("Current i: ", test[i])
-
     if((test[i] == "+" or test[i] == "-") and i != 0):
         fim = i
         numero = test[comeco:fim]
         clean.append(int(numero))
-        # print("Achou numero: ", numero)
-        clean.append(test[i]) #deveria ser um operador
-        # print("Achou operador: ", test[i])
+        clean.append(test[i]) 
         comeco = i+1
-        # print("Clean: ", clean)
-
     elif(i == len(test)-1):
-        # print("Last")
         clean.append(int(test[comeco:(len(test))]))
-
     else:
         pass    
 
+print(clean)
 res = 0
-
 test_char = clean
 
-if(test[-1] == "+" or test[-1] == "-"):
+if(test_char[-1] == "+" or test_char[-1] == "-"):
     raise ValueError("Inválido, último carácter não pode ser sinal.")
+
+if(len(test_char) < 3):
+    raise ValueError("Equação Inválida.")
 
 while (len(test_char) > 0):
     num1 = int(test_char[0])
     operador = test_char[1]
     num2 = int(test_char[2])
-    # print(f"conta: {num1} {operador} {num2}")
+    
     if operador == "+":
         res = (num1 + num2)
     elif operador == "-":
         res = (num1 - num2)
-    # print("Before: ", test_char)
-    del test_char[0]
-    del test_char[0]
-    del test_char[0]
-    # print("After: ", test_char)
-    # print("Res: ", res)
 
+    del test_char[0]
+    del test_char[0]
+    del test_char[0]
+    
     if(len(test_char) > 0):
         test_char = [res] + test_char
 
