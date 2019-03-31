58f383bb3f12cbee654d0d0c9b340276de09fa2f
Raphael Costa <rapha2706@hotmai..com>
2019-03-31 20:12:04 -0300

    Adicionando DS e EBNF atualizados

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # raphael_compiler
 Repositório para desenvolvimento do compilador da matéria de Lógica da Computação.
-![Diagrama Sintatico](https://github.com/raphacosta27/raphael_compiler/blob/master/DS3.png)
+![Diagrama Sintatico, parte 1](https://github.com/raphacosta27/raphael_compiler/blob/master/DSp1.png)
+![Diagrama Sintatico, parte 2](https://github.com/raphacosta27/raphael_compiler/blob/master/DSp2.png)
---
+++ helper.py
@@ -0,0 +1,246 @@
+import re
+class Node: #abstract
+    def __init__(self):
+        self.value = None
+        self.children = None
+    
+    def Evaluate(self):
+        pass
+
+class BinOp(Node):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+
+    def Evaluate(self):
+        if(self.value == "PLUS"):
+            return self.children[0].Evaluate() + self.children[1].Evaluate()
+            
+        elif(self.value == "MINUS"):
+            return self.children[0].Evaluate() - self.children[1].Evaluate()
+        
+        elif(self.value == "MULT"):
+            return self.children[0].Evaluate() * self.children[1].Evaluate()
+        
+        elif(self.value == "DIV"):
+            return self.children[0].Evaluate() // self.children[1].Evaluate()
+
+class UnOp(Node):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+
+    def Evaluate(self):
+        if(self.value == "MINUS"):
+            return -self.children[0].Evaluate()
+
+        if(self.value == "PLUS"):
+            return +self.children[0].Evaluate()
+
+class IntVal(Node):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+    
+    def Evaluate(self):
+        return self.value
+
+class NoOp(Node):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+    
+    def Evaluate(self):
+        pass
+
+class Token:
+    def __init__(self, t, v,):
+        self.type = t
+        self.value = v
+
+class Tokenizer:
+    def __init__(self, origin):
+        self.origin = origin
+        self.position = 0
+        self.actual = self.selectNext()
+
+    def selectNext(self):
+        current_token = ""
+        isNumber = False
+
+        while ( (self.position < len(self.origin)) and self.origin[self.position] == ' '):
+            self.position += 1
+
+        if(self.position == len(self.origin)):
+            new_token = Token("EOF", "EOF")
+            self.actual = new_token
+            return new_token
+
+        while ( (self.position < len(self.origin) ) and (self.origin[self.position]).isdigit()):
+            current_token += self.origin[self.position]
+            self.position += 1
+
+        while ( (self.position < len(self.origin) ):
+            if(self.origin[self.origin].isdigit()):
+                #identifica numero
+            elif(self.origin[self.origin].isalpha()):
+                #identifica variable
+            elif    
+        
+        if(len(current_token) == 0):
+            aux = self.origin[self.position]
+            if(aux == "+"):
+                new_token = Token("PLUS", "+")
+                self.position += 1
+                self.actual = new_token
+                return new_token
+
+            elif(aux == "-"):
+                new_token = Token("MINUS", "-")
+                self.position += 1
+                self.actual = new_token
+                return new_token
+
+            elif(aux == "/"):
+                new_token = Token("DIV", "/")
+                self.position += 1
+                self.actual = new_token
+                return new_token
+
+            elif(aux == "*"):
+                new_token = Token("MULT", "*")
+                self.position += 1
+                self.actual = new_token
+                return new_token
+
+            elif(aux == "("):
+                new_token = Token("(", "(")
+                self.position += 1
+                self.actual = new_token
+                return new_token
+
+            elif(aux == ")"):
+                new_token = Token(")", ")")
+                self.position += 1
+                self.actual = new_token 
+                return new_token
+
+            elif(aux == "="):
+                new_token = Token("ASSIGNMENT", "=")
+                self.position += 1
+                self.actual = new_token 
+                return new_token
+
+            else:
+                raise ValueError("Invalid Token", aux)
+
+        else:
+            if(isNumber):
+                new_token = Token("INT", int(current_token))
+                self.actual = new_token
+                return new_token
+            else:
+                if(current_token.upper() == "BEGIN"):
+                    new_token = Token("BEGIN", "BEGIN")
+                    self.actual = new_token
+                    return new_token
+                elif(current_token.upper() == "END"):
+                    new_token = Token("END", "END")
+                    self.actual = new_token
+                    return new_token
+
+class Parser:
+    @staticmethod
+    def parseExpression():
+        res = Parser.parseTerm()
+        while(Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS"):
+            if(Parser.tokens.actual.type == "PLUS"):
+                Parser.tokens.selectNext()
+                children1 = Parser.parseTerm()
+                res = BinOp("PLUS", [res, children1])
+            elif(Parser.tokens.actual.type == "MINUS"):
+                Parser.tokens.selectNext()
+                children1 = Parser.parseTerm()
+                res = BinOp("MINUS", [res, children1])
+        return res
+
+    @staticmethod
+    def parseTerm():
+        res = Parser.parseFactor()
+        while(Parser.tokens.actual.type == "MULT" or Parser.tokens.actual.type == "DIV"):
+            if(Parser.tokens.actual.type == "MULT"):
+                Parser.tokens.selectNext()
+                children1 = Parser.parseFactor()
+                res = BinOp("MULT", [res, children1])
+            elif(Parser.tokens.actual.type == "DIV"):
+                Parser.tokens.selectNext()
+                children1 = Parser.parseFactor()
+                res = BinOp("DIV", [res, children1])
+        return res
+    
+    @staticmethod
+    def parseFactor():
+        # res = 0
+        if(Parser.tokens.actual.type == "INT"):
+            new_node = IntVal(Parser.tokens.actual.value, [])
+            Parser.tokens.selectNext()
+            return new_node
+
+        elif(Parser.tokens.actual.type == "("):
+            Parser.tokens.selectNext()
+            new_node = Parser.parseExpression()
+            if(Parser.tokens.actual.type == ")"):
+                Parser.tokens.selectNext()
+                return new_node
+            else:
+                raise ValueError("Invalid Sintax, Missing ')'")
+
+        elif(Parser.tokens.actual.type == "MINUS" or Parser.tokens.actual.type == "PLUS"):
+            if(Parser.tokens.actual.type == "MINUS"):
+                Parser.tokens.selectNext()
+                new_node = UnOp("MINUS", [Parser.parseFactor()])
+                return new_node
+
+            elif(Parser.tokens.actual.type == "PLUS"):
+                Parser.tokens.selectNext()
+                new_node = UnOp("PLUS", [Parser.parseFactor()])
+                return new_node
+            else:
+                raise ValueError("Invalid Token, expecting INT, received: ", Parser.tokens.actual.type)
+
+        else:
+            raise ValueError("Invalid Token: ", Parser.tokens.actual.type)
+            
+        
+    @staticmethod
+    def run(code):
+        code = PrePro.filter(code)
+        # print(len(code))
+        Parser.tokens = Tokenizer(code)
+        res = Parser.parseExpression()
+        if(Parser.tokens.actual.type == "EOF"):
+            return res
+        else:
+            raise ValueError("Error: Unexpected token")
+class PrePro():
+    @staticmethod
+    def filter(text):
+        code = re.sub("'.*\n", "", data)
+        print(code)
+        return code
+
+# class SymbolTable():
+    
+
+teste = input("Filename: ")
+with open(f"{teste}.vbs") as file:
+    data = file.read()
+
+print(Parser.run(data).Evaluate())
+
+# tokenizer 
+# Parser
+# node statments 0 ou + filhos
+# node assigment 2 filhos
+# node identifier 0 filhos
+# isalpha
